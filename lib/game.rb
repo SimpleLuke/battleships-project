c048ec91b12d789f8f8e7efed6f2cb4f6b89dcac
Luke Lai <lukelai.dev@gmail.com>
2023-01-27 16:13:39 +0000

    Update

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,14 @@
 class Game
+  def initialize
+    @board = [
+      ['x','x','x','x','x'],
+      ['x','x','x','x','x'],
+      ['x','x','x','x','x'],
+      ['x','x','x','x','x'],
+      ['x','x','x','x','x'],
+    ]
+    
+  end
   def unplaced_ships
     ['###','####','#####']
   end
@@ -12,10 +22,30 @@ class Game
   end
 
   def place_ship(length:, orientation:, row:, col:)
+    # position = [row,col]
+    # if length > 1
+      count = 0
+      while count < length
+        if orientation == :horizontal
+          @board[row-1][col-1+count] = 'S'
+        else
+          @board[row-1+count][col-1] = 'S'
+        end
+        count += 1
+      end
+    # @board[col-1][row-1] = 'S'
+    p @board
     return {length:length, orientation: orientation, row: row, col: col}
   end
 
-  def ship_at?(x, y)
-    false
+  def ship_at?(row, col)
+    # p @board
+    if @board[col-1][row-1] == 'S'
+      # p 'TRUE'
+      return true
+    else
+      # p 'FALSE'
+      return false
+    end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -19,6 +19,18 @@ describe Game do
     expect(result).to eq(length: 3, orientation: :vertical, row: 3, col: 3) 
   end
 
+  it 'places a ship on (1,1) and returns true when checking' do
+    place = @game.place_ship(length: 1, orientation: :vertical, row: 1, col: 1)
+    expect(@game.ship_at?(1,1)).to eq true
+  end
+
+  it 'places a 1x2 ship on the board' do
+    place = @game.place_ship(length: 2, orientation: :horizontal, row: 1, col: 1)
+  expect(@game.ship_at?(1,1)).to eq true
+  expect(@game.ship_at?(2,1)).to eq true
+    
+  end
+
   it 'returns the number of rows of the board' do
     expect(@game.rows).to eq 5
   end
